041dd28c1b92cc987a47cc7cc78dbeef6be8a655
jquinteromo <qjaider1111@gmail.com>
2025-09-04 19:10:33 -0500

    Tocken add

--- storage/upload.ts
+++ storage/upload.ts
@@ -12,7 +12,7 @@ export async function uploadImage(file: File) {
   // 🔄 Crear cliente Supabase con header Authorization
   const supabase = createClient(
     'https://vjgnhuivftfesgcjdzrp.supabase.co',
-    'public-anon-key', // Usa variable de entorno en producción
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqZ25odWl2ZnRmZXNnY2pkenJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwMjAxNDUsImV4cCI6MjA3MjU5NjE0NX0.qbHRNtmiwly9Z0fOyL7EJxH6Uw0_woL_WQ26R01h1pw', // Usa variable de entorno en producción
     {
       global: {
         headers: {
